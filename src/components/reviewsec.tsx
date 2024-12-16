019ce6c63c4c6de1a8a4b8ee02c7101b028a6146
daniyalismail <daniyalismail19@gmail.com>
2024-12-16 23:23:03 +0500

    update reviewsec.tsx

--- src/components/reviewsec.tsx
+++ src/components/reviewsec.tsx
@@ -18,17 +18,17 @@ export default function Reviewsec(){
                 <div className="boxs1">
                 <Image src="/reviewpicture.png" height={22.58} width={138.84} alt="review"/>
                 <h1>Sarah M. <span><RiVerifiedBadgeFill/></span> </h1>
-                <p>"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
+                <p>Im blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece Ive bought has exceeded my expectations.</p>
                 </div>
                 <div className="boxs2">
                 <Image src="/reviewpicture.png" height={22.58} width={138.84} alt="review"/>
                 <h1>Alex K. <RiVerifiedBadgeFill/> </h1>
-                <p>"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”</p>
+                <p>Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”</p>
                     </div>
                     <div className="boxs3">
                     <Image src="/reviewpicture.png" height={22.58} width={138.84} alt="review"/>
                 <h1>James L. <RiVerifiedBadgeFill/> </h1>
-                <p>"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”</p>
+                <p>As someone whos always on the lookout for unique fashion pieces, Im thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.</p>
                     </div>
                     </div>
             </div>
